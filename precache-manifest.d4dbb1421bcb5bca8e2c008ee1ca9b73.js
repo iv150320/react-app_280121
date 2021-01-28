f2a403edc51aee14a2ea63c7ec7916464125ab12
iv150320 <johnrodin1@gmail.com>
2021-01-28 11:32:13 +0300

    Приложение по погоде версия от 280121

---
+++ precache-manifest.d4dbb1421bcb5bca8e2c008ee1ca9b73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c50235ba7690b80b3a9d743e2c29e9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f5507d5ae63d53bcbd5",
+    "url": "./static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "607dea452e1b4e2b46f1",
+    "url": "./static/css/main.5b6e81ca.chunk.css"
+  },
+  {
+    "revision": "9f5507d5ae63d53bcbd5",
+    "url": "./static/js/2.18493a4d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18493a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607dea452e1b4e2b46f1",
+    "url": "./static/js/main.630c9033.chunk.js"
+  },
+  {
+    "revision": "bd794c7b53e10c5f0bba",
+    "url": "./static/js/runtime-main.873cd961.js"
+  }
+]);
